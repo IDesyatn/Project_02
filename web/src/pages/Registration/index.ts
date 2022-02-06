ad4e879a7a2b4fb0d80bb0b58d38788bda5dd6b5
Vasulyik Anatolii <tolikvasulyk@ukr.net>
2022-02-06 19:36:12 +0200

    layout reg page(dark mode) - add showPass function.

--- web/src/pages/Registration/index.ts
+++ web/src/pages/Registration/index.ts
@@ -1,1 +1,16 @@
 import "./style.scss"
+
+/*
+const x = document.getElementById("password-input")! as HTMLInputElement;
+const y = document.getElementById("img")! as HTMLInputElement;
+
+function showPass() {
+    if (x.type === "password") {
+        x.type = "text";
+        y.src = "png"
+
+    } else {
+        x.type = "password";
+        y.src = "png"
+    }
+}*/
