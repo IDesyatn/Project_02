3a702a79fcdc5dab3dec41f206d39fd10c1ba5a0
Iryna Desyatnik <ira10nik385@gmail.com>
2022-02-14 20:31:45 +0200

    transfer logic to ts files, fix webpack

--- packages/web/src/pages/Login/index.ts
+++ packages/web/src/pages/Login/index.ts
@@ -1,16 +1,40 @@
 import "./style.scss"
 
+const buttonEye = document.getElementById('img');
+const dropdaunTheme = document.getElementById('select-mode');
 
-//выбивает ошибку при сборке вебпака
-/*console.log('login23423423423423')
+buttonEye.addEventListener('click', event => {
+    showPass()
+});
 
-class Test{
-    private test1: any;
-    private test2: any;
-    constructor(test1,test2) {
-        this.test1 = test1;
-        this.test2 = test2;
+dropdaunTheme.addEventListener('change', event => {
+    mode();
+});
+
+function showPass() {
+            const input  = <HTMLInputElement>document.getElementById("password-input");
+            const button  = <HTMLInputElement>document.getElementById("img");
+            if (input.type === "password") {
+                input.type = "text";
+                button.src = "https://img.icons8.com/material/24/ffffff/visible--v1.png"
+
+            } else {
+                input.type = "password";
+                button.src = "https://img.icons8.com/material-rounded/24/ffffff/sleepy-eyes.png"
+            }
+        }
+
+function mode() {
+            const a = ((document.getElementById('select-mode'))as HTMLInputElement).value;
+            const b = document.getElementById('box')
+            const c = document.getElementById('container')
+
+            if (a === "value2") {
+                b.classList.add("ligth");
+                c.classList.add("ligth-container");
+            } else {
+                b.classList.remove("ligth");
+                c.classList.remove("ligth-container");
+            }
+
     }
-}
-
-console.log(Test)*/
