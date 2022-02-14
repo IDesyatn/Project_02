918771b68329baa666ca77fc7a4f7ac8bb8a96e5
Vasulyik Anatolii <tolikvasulyk95@gmail.com>
2022-02-15 01:59:56 +0200

    layout enter pages (login,registration) - language change

--- packages/web/src/pages/Login/index.ts
+++ packages/web/src/pages/Login/index.ts
@@ -2,6 +2,7 @@ import "./style.scss"
 
 const buttonEye = document.getElementById('img');
 const dropdaunTheme = document.getElementById('select-mode');
+const changeLange = document.getElementById('select-lang');
 
 buttonEye.addEventListener('click', event => {
     showPass()
@@ -38,3 +39,33 @@ function mode() {
             }
 
     }
+
+
+
+
+changeLange.onchange = function(){
+  if (changeLange[1].selected === true){
+    document.getElementById('box__title').innerText = 'Вход';
+    document.getElementById('them-1').innerText = 'Темная';
+    document.getElementById('them-2').innerText = 'Светлая';
+    document.getElementById('submit').value = 'Вход';
+    document.getElementById('warning').innerText = 'Нужен аккаунт?';
+    document.getElementById('warning-link').innerText = 'Регистрация';
+
+    document.getElementById('box__title-1').innerText = 'Логин';
+    document.getElementById('box__pass-title-1').innerText = 'Пароль';
+    document.getElementById('box__pass-title-2').innerText = 'Подтверждение пароля';
+  }else {
+    document.getElementById('box__title').innerText = 'Login';
+    document.getElementById('them-1').innerText = 'Dark';
+    document.getElementById('them-2').innerText = 'Light';
+    document.getElementById('submit').value = 'Log in';
+    document.getElementById('warning').innerText = 'Need an account?';
+    document.getElementById('warning-link').innerText = 'Sign in.';
+
+    document.getElementById('box__title-1').innerText = 'Login';
+    document.getElementById('box__pass-title-1').innerText = 'PASSWORD';
+    document.getElementById('box__pass-title-2').innerText = 'CONFIRM PASSWORD';
+  }
+
+}
--- packages/web/src/pages/Registration/index.ts
+++ packages/web/src/pages/Registration/index.ts
@@ -3,6 +3,7 @@ import "./style.scss"
 const buttonEye = document.getElementById('img');
 const buttonEye2 = document.getElementById('img2');
 const dropdaunTheme = document.getElementById('select-mode');
+const changeLange = document.getElementById('select-lang');
 
 buttonEye.addEventListener('click', event => {
     showPass('password-input', 'img');
@@ -43,3 +44,31 @@ function mode() {
             }
 
     }
+
+
+changeLange.onchange = function(){
+  if (changeLange[1].selected === true){
+    document.getElementById('box__title').innerText = 'Регистрация';
+    document.getElementById('them-1').innerText = 'Темная';
+    document.getElementById('them-2').innerText = 'Светлая';
+    document.getElementById('submit').value = 'Регистрация';
+    document.getElementById('warning').innerText = 'У вас уже есть аккаунт?';
+    document.getElementById('warning-link').innerText = 'Вход';
+
+    document.getElementById('box__title-1').innerText = 'Логин';
+    document.getElementById('box__pass-title-1').innerText = 'Пароль';
+    document.getElementById('box__pass-title-2').innerText = 'Подтверждение пароля';
+  }else {
+    document.getElementById('box__title').innerText = 'Registration';
+    document.getElementById('them-1').innerText = 'Dark';
+    document.getElementById('them-2').innerText = 'Light';
+    document.getElementById('submit').value = 'Register';
+    document.getElementById('warning').innerText = 'Need an account?';
+    document.getElementById('warning-link').innerText = 'Sign up.';
+
+    document.getElementById('box__title-1').innerText = 'Login';
+    document.getElementById('box__pass-title-1').innerText = 'PASSWORD';
+    document.getElementById('box__pass-title-2').innerText = 'CONFIRM PASSWORD';
+  }
+
+}
