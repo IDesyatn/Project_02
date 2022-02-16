991525d6a8ec4b9db922987381688eeaae997f1a
Olha Starosta <shop2029@gmail.com>
2022-02-16 14:51:34 +0200

    added some style

--- packages/web/src/pages/Login/index.ts
+++ packages/web/src/pages/Login/index.ts
@@ -1,9 +1,16 @@
 import "./style.scss"
-import {showPass, mode, loginValidation,validateStatusCheck,passwordValidation} from "./logic" 
+import { languageHandle }  from '../../ts/localization'
+
+
+document.addEventListener("DOMContentLoaded", () => {
+languageHandle ();
+});
+
+import { showPass, mode, loginValidation, validateStatusCheck, passwordValidation } from "./logic"
 
 const buttonEye = document.getElementById('img');
 const dropdaunTheme = document.getElementById('select-mode');
-const changeLange = document.getElementById('select-lang');
+// const changeLange = document.getElementById('select-lang');
 
 buttonEye.addEventListener('click', event => {
     showPass()
@@ -14,63 +21,63 @@ dropdaunTheme.addEventListener('change', event => {
 });
 
 
-function showPass() {
-            const input  = <HTMLInputElement>document.getElementById("password-input");
-            const button  = <HTMLInputElement>document.getElementById("img");
-            if (input.type === "password") {
-                input.type = "text";
-                button.src = "https://img.icons8.com/material/24/ffffff/visible--v1.png"
+// function showPass() {
+//             const input  = <HTMLInputElement>document.getElementById("password-input");
+//             const button  = <HTMLInputElement>document.getElementById("img");
+//             if (input.type === "password") {
+//                 input.type = "text";
+//                 button.src = "https://img.icons8.com/material/24/ffffff/visible--v1.png"
+//
+//             } else {
+//                 input.type = "password";
+//                 button.src = "https://img.icons8.com/material-rounded/24/ffffff/sleepy-eyes.png"
+//             }
+//         }
+//
+// function mode() {
+//             const a = ((document.getElementById('select-mode'))as HTMLInputElement).value;
+//             const b = document.getElementById('box')
+//             const c = document.getElementById('container')
+//
+//             if (a === "value2") {
+//                 b.classList.add("ligth");
+//                 c.classList.add("ligth-container");
+//             } else {
+//                 b.classList.remove("ligth");
+//                 c.classList.remove("ligth-container");
+//             }
+//
+//     }
+//
+//
 
-            } else {
-                input.type = "password";
-                button.src = "https://img.icons8.com/material-rounded/24/ffffff/sleepy-eyes.png"
-            }
-        }
 
-function mode() {
-            const a = ((document.getElementById('select-mode'))as HTMLInputElement).value;
-            const b = document.getElementById('box')
-            const c = document.getElementById('container')
-
-            if (a === "value2") {
-                b.classList.add("ligth");
-                c.classList.add("ligth-container");
-            } else {
-                b.classList.remove("ligth");
-                c.classList.remove("ligth-container");
-            }
-
-    }
-
-
-
-
-changeLange.onchange = function(){
-  if (changeLange[1].selected === true){
-    document.getElementById('box__title').innerText = 'Вход';
-    document.getElementById('them-1').innerText = 'Темная';
-    document.getElementById('them-2').innerText = 'Светлая';
-    document.getElementById('submit').value = 'Вход';
-    document.getElementById('warning').innerText = 'Нужен аккаунт?';
-    document.getElementById('warning-link').innerText = 'Регистрация';
-
-    document.getElementById('box__title-1').innerText = 'Логин';
-    document.getElementById('box__pass-title-1').innerText = 'Пароль';
-    document.getElementById('box__pass-title-2').innerText = 'Подтверждение пароля';
-  }else {
-    document.getElementById('box__title').innerText = 'Login';
-    document.getElementById('them-1').innerText = 'Dark';
-    document.getElementById('them-2').innerText = 'Light';
-    document.getElementById('submit').value = 'Log in';
-    document.getElementById('warning').innerText = 'Need an account?';
-    document.getElementById('warning-link').innerText = 'Sign in.';
-
-    document.getElementById('box__title-1').innerText = 'Login';
-    document.getElementById('box__pass-title-1').innerText = 'PASSWORD';
-    document.getElementById('box__pass-title-2').innerText = 'CONFIRM PASSWORD';
-  }
-
-}
+// changeLange.onchange = function(){
+//   if (changeLange[1].selected === true){
+//     document.getElementById('box__title').innerText = 'Вход';
+//     document.getElementById('them-1').innerText = 'Темная';
+//     document.getElementById('them-2').innerText = 'Светлая';
+//     document.getElementById('submit').value = 'Вход';
+//     document.getElementById('warning').innerText = 'Нужен аккаунт?';
+//     document.getElementById('warning-link').innerText = 'Регистрация';
+//
+//     document.getElementById('box__title-1').innerText = 'Логин';
+//     document.getElementById('box__pass-title-1').innerText = 'Пароль';
+//     document.getElementById('box__pass-title-2').innerText = 'Подтверждение пароля';
+//   }else {
+//     document.getElementById('box__title').innerText = 'Login';
+//     document.getElementById('them-1').innerText = 'Dark';
+//     document.getElementById('them-2').innerText = 'Light';
+//     document.getElementById('submit').value = 'Log in';
+//     document.getElementById('warning').innerText = 'Need an account?';
+//     document.getElementById('warning-link').innerText = 'Sign in.';
+//
+//     document.getElementById('box__title-1').innerText = 'Login';
+//     document.getElementById('box__pass-title-1').innerText = 'PASSWORD';
+//     document.getElementById('box__pass-title-2').innerText = 'CONFIRM PASSWORD';
+//   }
+//
+// }
 
 function addListener(id, eventType, callback) {
   const node = document.getElementById(id);
---
+++ packages/web/src/ts/localization.ts
@@ -0,0 +1,125 @@
+export function languageHandle () {
+  const selectedLanguage: HTMLElement = document.getElementById('select-language');
+  const translateElements: any = document.querySelectorAll<HTMLElement>('[data-translate]');
+  const placeholderElements: any = document.querySelectorAll<HTMLElement>('[data-placeholder]');
+
+  const ruTranslations = {
+    firstName: `Имя`,
+    lastName: 'Фамилия',
+    age: 'Возраст',
+    city: 'Город',
+    phone: 'Телефон',
+    email: 'Електронная почта',
+    company: 'Компания',
+    create: 'Создать',
+    update: 'Изменить',
+    clearAll: 'Очистить все',
+    english: 'Англ',
+    russian: 'Рус',
+    light: 'Светлая',
+    dark: 'Тёмная',
+    settings: 'Настройки',
+    settingsM: 'НАСТРОЙКИ',
+    login: 'Логин',
+    loginUC: 'ЛОГИН',
+    logout: 'Выйти',
+    confirm: 'Подтвердить',
+    searchFName: "Поиск: по имени или фамилии",
+    searchFirstName: 'введите имя или фамилию...',
+    cancel: 'Отменить',
+    delete: 'Удалить',
+    currentPassword: 'Текущий пароль',
+    newPassword: 'Новый пароль',
+    passwordUC: 'ПАРОЛЬ',
+    password: 'Пароль',
+    yes: 'ДА',
+    no: 'НЕТ',
+    doYouWant: 'Вы хотите удалить выделеную строку таблицы?',
+    account: 'У вас уже есть аккаунт?',
+    signIn: 'Вход',
+    username: 'Имя пользователя',
+    needAccount: 'Нужен аккаунт?',
+    logIn: 'ВХОД',
+    registration: 'Регистрация',
+    confPas: 'Подтверждение пароля',
+    fieldEmpty:'Поле не может быть пустым',
+    loginLength: 'Логин не менее 6 символов',
+  };
+  const enTranslations = {
+    firstName: 'First name',
+    lastName: 'Last name',
+    age: 'Age',
+    city: 'City',
+    phone: 'Phone',
+    email: 'Email',
+    company: 'Company',
+    create: 'Create',
+    update: 'Update',
+    delete: 'DELETE',
+    clearAll: 'Clear All',
+    english: 'Eng',
+    russian: 'Rus',
+    light: 'Light',
+    dark: 'Dark',
+    settings: 'Settings',
+    settingsM: 'SETTINGS',
+    logout: 'Logout',
+    confirm: 'CONFIRM',
+    cancel: 'CANCEL',
+    login: 'Login',
+    loginUC: 'LOGIN',
+    yourPassword: 'Current password',
+    newPassword: 'New password',
+    passwordUC: 'PASSWORD',
+    password: 'Password',
+    searchFName: "Search: First or Last name",
+    searchFirstName: 'enter first or last name...',
+    yes: 'YES',
+    no: 'NO',
+    doYouWant: 'Do you want to delete the selected row in the table?',
+    account: 'Already have an account?',
+    signIn: 'Sign in',
+    username: 'Username',
+    needAccount: 'Need an account?',
+    logIn: 'LOG IN',
+    registration: 'Registration',
+    confPas: 'CONFIRM PASSWORD',
+    fieldEmpty:'Field can`t be empty',
+    loginLength: 'Login at least 6 characters',
+    loginLen: 'Login can`t be longer than 20 characters',
+    loginValid: 'Login must contain only letters, numbers, and underscores'
+  };
+
+  const localStorageLanguage: string = localStorage.getItem('selected-language') || 'english';
+  selectedLanguage.value = localStorageLanguage;
+  setLanguage(localStorageLanguage);
+
+  selectedLanguage.addEventListener('change', (event) => {
+    setLanguage(selectedLanguage.value);
+  })
+
+  function setLanguage(lang) {
+    let translation = getTranslation(lang);
+    localStorage.setItem('selected-language', lang);
+
+    translateElements.forEach((el) => {
+      const translateKey = el.dataset.translate;
+      el.textContent = translation[translateKey];
+    });
+    placeholderElements.forEach((el) => {
+      const placeholderKey = el.dataset.placeholder;
+      el.placeholder =  translation[placeholderKey];
+    })
+  }
+
+  function getTranslation(lang) {
+    switch (lang) {
+      case 'russian':
+        return ruTranslations;
+      case 'english':
+        return enTranslations;
+      default:
+        return enTranslations;
+    }
+  }
+}
